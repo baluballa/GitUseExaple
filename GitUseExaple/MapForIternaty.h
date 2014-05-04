717e2b0c504e1fe9f91c6cd7b52e7b4df1c713c0
Thor Vik <developer@wingthor.net>
2014-05-04 20:02:36 +0200

    Created new classes for map feature

---
+++ GitUseExaple/MapForIternaty.h
@@ -0,0 +1,13 @@
+//
+//  MapForIternaty.h
+//  GitUseExaple
+//
+//  Created by Thor Vik on 04.05.14.
+//  Copyright (c) 2014 Thor Vik Consulting. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForIternaty : NSObject
+
+@end
---
+++ GitUseExaple/MapForIternaty.m
@@ -0,0 +1,13 @@
+//
+//  MapForIternaty.m
+//  GitUseExaple
+//
+//  Created by Thor Vik on 04.05.14.
+//  Copyright (c) 2014 Thor Vik Consulting. All rights reserved.
+//
+
+#import "MapForIternaty.h"
+
+@implementation MapForIternaty
+
+@end
